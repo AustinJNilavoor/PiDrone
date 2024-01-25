0b9d01bebce2bad7733df76b4fa800cd785c035f
Austin J Nilavoor <nilavooraustin@gamail.com>
2024-01-25 23:50:14 +0530

    serial dis

--- drone.c
+++ drone.c
@@ -90,8 +90,8 @@ int main()
         mpu6050_read_raw(acceleration, gyro);
         printf("Acc. X = %d, Y = %d, Z = %d , new\n", acceleration[0], acceleration[1], acceleration[2]);
         printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-        uart_read(data);
-        printf("%d %c",data[0],data[1]);
+        // uart_read(data);
+        // printf("%d %c",data[0],data[1]);
         sleep_ms(100);
     }
 }
