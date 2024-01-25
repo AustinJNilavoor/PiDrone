fd3767f0ba8e317953e51f804355718ac3355ed7
Austin J Nilavoor <nilavooraustin@gamail.com>
2024-01-25 19:07:56 +0530

    newcode

--- drone.c
+++ drone.c
@@ -1,74 +1,160 @@
-#include "pico/stdlib.h"
 #include <stdio.h>
-#include <string.h>
-#include "pico/binary_info.h"
+#include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-#define CWM_1 12
-#define CWM_2 21
-#define CCWM_1 3
-#define CCWM_2 16
+// I2C address
+static const uint8_t ADXL343_ADDR = 0x53;
 
-#define I2C_SDA_PIN 14
-#define I2C_SCL_PIN 15
+// Registers
+static const uint8_t REG_DEVID = 0x00;
+static const uint8_t REG_POWER_CTL = 0x2D;
+static const uint8_t REG_DATAX0 = 0x32;
 
+// Other constants
+static const uint8_t DEVID = 0xE5;
+static const float SENSITIVITY_2G = 1.0 / 256;  // (g/LSB)
+static const float EARTH_GRAVITY = 9.80665;     // Earth's gravity in [m/s^2]
 
-static int i2c_addr = 0x68;
+/*******************************************************************************
+ * Function Declarations
+ */
+int reg_write(i2c_inst_t *i2c, 
+                const uint addr, 
+                const uint8_t reg, 
+                uint8_t *buf,
+                const uint8_t nbytes);
 
-i2c_inst_t *i2c = i2c1;
+int reg_read(   i2c_inst_t *i2c,
+                const uint addr,
+                const uint8_t reg,
+                uint8_t *buf,
+                const uint8_t nbytes);
 
-static void mpu6050_reset()
-{
-    uint8_t buf[] = {0x6B, 0x80};
-    i2c_write_blocking(i2c, i2c_addr, buf, 2, false);
-}
+/*******************************************************************************
+ * Function Definitions
+ */
 
-static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp)
-{
-    uint8_t buffer[6];
-    uint8_t val = 0x3B;
+// Write 1 byte to the specified register
+int reg_write(  i2c_inst_t *i2c, 
+                const uint addr, 
+                const uint8_t reg, 
+                uint8_t *buf,
+                const uint8_t nbytes) {
 
-    i2c_write_blocking(i2c, i2c_addr, &val, 1, true);
-    i2c_read_blocking(i2c, i2c_addr, buffer, 6, false);
-    for (int i = 0; i < 3; i++)
-    {
-        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+    int num_bytes_read = 0;
+    uint8_t msg[nbytes + 1];
+
+    // Check to make sure caller is sending 1 or more bytes
+    if (nbytes < 1) {
+        return 0;
     }
 
-    val = 0x43;
-    i2c_write_blocking(i2c, i2c_addr, &val, 1, true);
-    i2c_read_blocking(i2c, i2c_addr, buffer, 6, false);
-
-    for (int i = 0; i < 3; i++)
-    {
-        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+    // Append register address to front of data packet
+    msg[0] = reg;
+    for (int i = 0; i < nbytes; i++) {
+        msg[i + 1] = buf[i];
     }
-    val = 0x41;
-    i2c_write_blocking(i2c, i2c_addr, &val, 1, true);
-    i2c_read_blocking(i2c, i2c_addr, buffer, 2, false);  // False - finished with bus
-    
-    *temp = buffer[0] << 8 | buffer[1];
+
+    // Write data to register(s) over I2C
+    i2c_write_blocking(i2c, addr, msg, (nbytes + 1), false);
+
+    return num_bytes_read;
 }
 
-int main()
-{
+// Read byte(s) from specified register. If nbytes > 1, read from consecutive
+// registers.
+int reg_read(  i2c_inst_t *i2c,
+                const uint addr,
+                const uint8_t reg,
+                uint8_t *buf,
+                const uint8_t nbytes) {
+
+    int num_bytes_read = 0;
+
+    // Check to make sure caller is asking for 1 or more bytes
+    if (nbytes < 1) {
+        return 0;
+    }
+
+    // Read data from register(s) over I2C
+    i2c_write_blocking(i2c, addr, &reg, 1, true);
+    num_bytes_read = i2c_read_blocking(i2c, addr, buf, nbytes, false);
+
+    return num_bytes_read;
+}
+
+/*******************************************************************************
+ * Main
+ */
+int main() {
+
+    int16_t acc_x;
+    int16_t acc_y;
+    int16_t acc_z;
+    float acc_x_f;
+    float acc_y_f;
+    float acc_z_f;
+
+    // Pins
+    const uint sda_pin = 16;
+    const uint scl_pin = 17;
+
+    // Ports
+    i2c_inst_t *i2c = i2c0;
+
+    // Buffer to store raw reads
+    uint8_t data[6];
+
+    // Initialize chosen serial port
     stdio_init_all();
-    sleep_ms(1000);
-    i2c_init(i2c, 400 * 1000);
-    gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA_PIN);
-    gpio_pull_up(I2C_SCL_PIN);
-    bi_decl(bi_2pins_with_func(I2C_SDA_PIN, I2C_SCL_PIN, GPIO_FUNC_I2C));
-    mpu6050_reset();
-    int16_t acceleration[3], gyro[3], temp;
 
-    while (1)
-    {
-        mpu6050_read_raw(acceleration, gyro, &temp);
-        printf("Acc. X = %d, Y = %d, Z = %d , new\n", acceleration[0], acceleration[1], acceleration[2]);
-        printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-        printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+    //Initialize I2C port at 400 kHz
+    i2c_init(i2c, 400 * 1000);
+
+    // Initialize I2C pins
+    gpio_set_function(sda_pin, GPIO_FUNC_I2C);
+    gpio_set_function(scl_pin, GPIO_FUNC_I2C);
+
+    // Read device ID to make sure that we can communicate with the ADXL343
+    reg_read(i2c, ADXL343_ADDR, REG_DEVID, data, 1);
+    if (data[0] != DEVID) {
+        printf("ERROR: Could not communicate with ADXL343\r\n");
+        while (true);
+    }
+
+    // Read Power Control register
+    reg_read(i2c, ADXL343_ADDR, REG_POWER_CTL, data, 1);
+    printf("0xX\r\n", data[0]);
+
+    // Tell ADXL343 to start taking measurements by setting Measure bit to high
+    data[0] |= (1 << 3);
+    reg_write(i2c, ADXL343_ADDR, REG_POWER_CTL, &data[0], 1);
+
+    // Test: read Power Control register back to make sure Measure bit was set
+    reg_read(i2c, ADXL343_ADDR, REG_POWER_CTL, data, 1);
+    printf("0xX\r\n", data[0]);
+
+    // Wait before taking measurements
+    sleep_ms(2000);
+
+    // Loop forever
+    while (true) {
+
+        // Read X, Y, and Z values from registers (16 bits each)
+        reg_read(i2c, ADXL343_ADDR, REG_DATAX0, data, 6);
+
+        // Convert 2 bytes (little-endian) into 16-bit integer (signed)
+        acc_x = (int16_t)((data[1] << 8) | data[0]);
+        acc_y = (int16_t)((data[3] << 8) | data[2]);
+        acc_z = (int16_t)((data[5] << 8) | data[4]);
+
+        // Convert measurements to [m/s^2]
+        acc_x_f = acc_x * SENSITIVITY_2G * EARTH_GRAVITY;
+        acc_y_f = acc_y * SENSITIVITY_2G * EARTH_GRAVITY;
+        acc_z_f = acc_z * SENSITIVITY_2G * EARTH_GRAVITY;
+
+        // Print results
+        printf("X: %.2f | Y: %.2f | Z: %.2f\r\n", acc_x_f, acc_y_f, acc_z_f);
 
         sleep_ms(100);
     }
