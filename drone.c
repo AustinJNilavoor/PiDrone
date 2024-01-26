7cdf11d405446cca4ab26ea00fca3da5de7e4b6a
Austin J Nilavoor <nilavooraustin@gamail.com>
2024-01-26 15:48:40 +0530

    i2c uart

--- drone.c
+++ drone.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "pico/binary_info.h"
 #include "hardware/uart.h"
 
-/// \tag::hello_uart[]
+#define CWM_1 12
+#define CWM_2 21
+#define CCWM_1 3
+#define CCWM_2 16
+
+#define SDA_PIN 14
+#define SCL_PIN 15
 
 #define UART_ID uart0
 #define BAUD_RATE 115200
 
-// We are using pins 0 and 1, but see the GPIO function select table in the
-// datasheet for information on which other pins can be used.
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
+
+#define I2C_ID i2c1
+#define I2C_ADDR 0x68
+
+int16_t acceleration[3], gyro[3];
+
 uint8_t ch;
 static void on_uart_rx()
 {
@@ -19,28 +31,65 @@ static void on_uart_rx()
     printf("%c", ch);
 }
 
+static void mpu6050_reset()
+{
+    uint8_t buf[] = {0x6B, 0x00};
+    i2c_write_blocking(I2C_ID, I2C_ADDR, buf, 2, false);
+}
+
+static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3])
+{
+    uint8_t buffer[6];
+    uint8_t val = 0x3B;
+
+    i2c_write_blocking(I2C_ID, I2C_ADDR, &val, 1, true);
+    i2c_read_blocking(I2C_ID, I2C_ADDR, buffer, 6, false);
+    for (int i = 0; i < 3; i++)
+    {
+        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+    }
+
+    val = 0x43;
+    i2c_write_blocking(I2C_ID, I2C_ADDR, &val, 1, true);
+    i2c_read_blocking(I2C_ID, I2C_ADDR, buffer, 6, false);
+
+    for (int i = 0; i < 3; i++)
+    {
+        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+    }
+}
+
+static void i2c_setup()
+{
+    i2c_init(I2C_ID, 400 * 1000);
+    gpio_set_function(SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(SDA_PIN);
+    gpio_pull_up(SCL_PIN);
+    bi_decl(bi_2pins_with_func(SDA_PIN, SCL_PIN, GPIO_FUNC_I2C));
+    mpu6050_reset();
+}
+
+static void uart_setup()
+{
+    uart_init(UART_ID, BAUD_RATE);
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    irq_set_exclusive_handler(UART0_IRQ, on_uart_rx);
+    irq_set_enabled(UART0_IRQ, true);
+    uart_set_irq_enables(UART_ID, true, false);
+}
+
 int main()
 {
     stdio_init_all();
-    // Set up our UART with the required speed.
-    uart_init(UART_ID, BAUD_RATE);
-
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-
-    // And set up and enable the interrupt handlers
-    irq_set_exclusive_handler(UART0_IRQ, on_uart_rx);
-    irq_set_enabled(UART0_IRQ, true);
-
-    // Now enable the UART to send interrupts - RX only
-    uart_set_irq_enables(UART_ID, true, false);
-uint32_t c = 0;
+    i2c_setup();
+    uart_setup();
     while (1)
     {
-        printf("Non intrep %d\n",c);
-        c++;
-        sleep_us(50000);
+        mpu6050_read_raw(acceleration, gyro);
+        printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
+        printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
+        sleep_us(100);
     }
 }
