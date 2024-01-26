ec82b43b1d32842c80019f60af40460051007add
Austin J Nilavoor <nilavooraustin@gamail.com>
2024-01-26 15:50:31 +0530

    uart tes

--- drone.c
+++ drone.c
@@ -26,9 +26,10 @@ int16_t acceleration[3], gyro[3];
 uint8_t ch;
 static void on_uart_rx()
 {
-
+printf("start");
     ch = uart_getc(UART_ID);
     printf("%c", ch);
+    printf("stp");
 }
 
 static void mpu6050_reset()
@@ -87,9 +88,9 @@ int main()
     uart_setup();
     while (1)
     {
-        mpu6050_read_raw(acceleration, gyro);
-        printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
-        printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
+        // mpu6050_read_raw(acceleration, gyro);
+        // printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
+        // printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
         sleep_us(100);
     }
 }
