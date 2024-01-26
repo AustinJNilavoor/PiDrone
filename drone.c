2a0dbfdc42fb9b18c287efe33836c1e4d569a49f
Austin J Nilavoor <nilavooraustin@gamail.com>
2024-01-26 10:04:36 +0530

    uart rea blo

--- drone.c
+++ drone.c
@@ -28,23 +28,16 @@ int main() {
     // Set datasheet for more information on function select
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
-
-    // Send out a character without any conversions
-    uart_putc_raw(UART_ID, 'A');
-
-    // Send out a character but do CR/LF conversions
-    uart_putc(UART_ID, 'B');
-
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
-    uint8_t ch;
+    uint8_t buff[10];
     while (1)
     {
-        ch = uart_getc(UART_ID);
-        printf("%c \n",ch);
+        if (uart_is_readable(UART_ID))
+        {
+            uart_read_blocking 	(UART_ID,buff,10);
+        }
+        
+        // ch = uart_getc(UART_ID);
+        printf("%s ee\n",buff);
         /* code */
     }
     
