5aacc3bf91727430546cddb07e484fea0784d57e
Austin J Nilavoor <nilavooraustin@gamail.com>
2024-01-26 10:10:09 +0530

    aa

--- drone.c
+++ drone.c
@@ -28,6 +28,18 @@ int main() {
     // Set datasheet for more information on function select
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    // Use some the various UART functions to send out data
+    // In a default system, printf will also output via the default UART
+
+    // Send out a character without any conversions
+    uart_putc_raw(UART_ID, 'A');
+
+    // Send out a character but do CR/LF conversions
+    uart_putc(UART_ID, 'B');
+
+    // Send out a string, with CR/LF conversions
+    uart_puts(UART_ID, " Hello, UART!\n");
     uint8_t ch;
     while (1)
     {
