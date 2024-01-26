e0c3bd95f2b4f9f8bfe1e6f8557dcd94dc452165
Austin J Nilavoor <nilavooraustin@gamail.com>
2024-01-26 15:25:26 +0530

    intrept uart

--- drone.c
+++ drone.c
@@ -11,6 +11,13 @@
 // datasheet for information on which other pins can be used.
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
+uint8_t ch;
+static void on_uart_rx()
+{
+
+    ch = uart_getc(UART_ID);
+    printf("%c", ch);
+}
 
 int main()
 {
@@ -22,11 +29,17 @@ int main()
     // Set datasheet for more information on function select
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    uint8_t ch;
+
+    // And set up and enable the interrupt handlers
+    irq_set_exclusive_handler(UART0_IRQ, on_uart_rx);
+    irq_set_enabled(UART0_IRQ, true);
+
+    // Now enable the UART to send interrupts - RX only
+    uart_set_irq_enables(UART_ID, true, false);
+
     while (1)
     {
-        ch = uart_getc(UART_ID);
-        printf("%c\n", ch);
+        printf("Non intrep");
         sleep_us(100);
     }
 }
